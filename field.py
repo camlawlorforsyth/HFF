c7cd913b0a29daa1096e5754f1d76f7e13c13e23
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2021-04-06 21:03:25 -0500

    major updates

--- field.py
+++ field.py
@@ -1,4 +1,5 @@
 
+import os
 import errno
 import numpy as np
 
@@ -10,119 +11,129 @@ from astropy.wcs import WCS
 
 from core import save_cutout
 
-class Cluster :
+def determine_rms(segPath, files) :
+    '''
+    Estimate the RMS noise level in each flter image by using the
+    segmentation map as a basic mask. Mask all the pixels that are
+    associated with an object and determine the noise level from the
+    remaining background pixels.
     
-    def __init__(self, name, filters, files, segPath, finalObjsPath, rms) :
-        self.name = name
-        self.filters = filters
-        self.files = files
-        self.segPath = segPath
-        self.finalObjsPath = finalObjsPath
-        self.rms = rms
+    Returns
+    -------
+    rmses : list
+        List of rms values, one for each filter.
     
-    def determine_rms(self) :
-        '''
-        Estimate the RMS noise level in each flter image by using the
-        segmentation map as a basic mask. Mask all the pixels that are
-        associated with an object and determine the noise level from the
-        remaining background pixels.
+    '''
+    
+    with fits.open(segPath) as hdu :
+        segMap = hdu[0].data
+    
+    rmses = []
+    for file in files :
+        with fits.open(file) as hdu :
+            sci = hdu[0].data
         
-        Returns
-        -------
-        rmses : list
-            List of rms values, one for each filter.
-        
-        '''
-        
-        with fits.open(self.segPath) as hdu :
-            segMap = hdu[0].data
-        
-        rmses = []
-        for file in self.files :
-            with fits.open(file) as hdu :
-                sci = hdu[0].data
+        mask = np.where(segMap == 0)
+        masked_sci = sci[mask]
+        masked_sci[masked_sci > 10] = np.nan
+        rms = np.sqrt(np.nanmean(np.square(masked_sci)))
+        rmses.append(rms)
+    
+    return rmses
+
+def save_cutouts(cluster, final_objs_path, filters, rms, files, segPath) :
+    '''
+    Save the cutouts for all science objects in a given filter, as well as
+    noise cutouts. Then move to the next filter, saving those cutouts. If
+    there are N filters and M final science objects, this will result in
+    2*N*M cutout images.
+    
+    Returns
+    -------
+    None.
+    
+    '''
+    
+    extent = 5 # maximum radius of image will be 5 times R_e
+    
+    sci_objs = Table.read(final_objs_path)
+    sci_objs = sci_objs[np.where(sci_objs['id'] == 5830)] # for testing
+    ID, r_e = sci_objs['id'], sci_objs['flux_radius']*u.pixel
+    ra, dec, redshift = sci_objs['ra'], sci_objs['dec'], sci_objs['z']
+    sma, smb = sci_objs['a_image'], sci_objs['b_image']
+    pa = sci_objs['theta_J2000']
+    
+    dictionary = {}
+    for i in range(len(filters)) :
+        dictionary[filters[i]] = {'rms':rms[i], 'file':files[i]}
+    
+    os.makedirs('{}/cutouts'.format(cluster), exist_ok=True) # ensure the
+        # output directory for the cutouts is available
+    
+    for filt in dictionary :
+        with fits.open(dictionary.get(filt).get('file')) as hdu :
+            hdr = hdu[0].header
+            exposure = hdr['EXPTIME']*u.s
+            # photfnu = hdr['PHOTFNU'] # the ACS images don't have PHOTFNU
+            photflam = hdr['PHOTFLAM']*u.erg/(u.cm**2)/u.AA/u.electron
+            photplam = hdr['PHOTPLAM']*u.AA
+            photfnu = ((photplam**2)*
+                       photflam/const.c).to(u.Jy*u.s/u.electron)
+            scale = hdr['D001SCAL']*u.arcsec/u.pixel
+            wcs = WCS(hdr)
+            science = hdu[0].data
             
-            mask = np.where(segMap == 0)
-            masked_sci = sci[mask]
-            masked_sci[masked_sci > 10] = np.nan
-            rms = np.sqrt(np.nanmean(np.square(masked_sci)))
-            rmses.append(rms)
-        
-        return rmses
-    
-    def save_cutouts(self) :
-        '''
-        Save the cutouts for all science objects in a given filter, as well as
-        noise cutouts. Then move to the next filter, saving those cutouts. If
-        there are N filters and M final science objects, this will result in
-        2*N*M cutout images.
-        
-        Returns
-        -------
-        None.
-        
-        '''
-        
-        extent = 5 # maximum radius of image will be 5 times R_e
-        
-        sciObjs = Table.read(self.finalObjsPath)
-        sciObjs = sciObjs[np.where(sciObjs['id'] == 5830)] # for testing
-        ID, r_e = sciObjs['id'], sciObjs['flux_radius']*u.pixel
-        ra, dec, redshift = sciObjs['ra'], sciObjs['dec'], sciObjs['z']
-        sma, smb = sciObjs['a_image'], sciObjs['b_image']
-        pa = sciObjs['theta_J2000']
-        
-        dictionary = {}
-        for i in range(len(self.filters)) :
-            dictionary[self.filters[i]] = {'rms':self.rms[i],
-                                           'file':self.files[i]}
-        
-        for filt in dictionary :
-            with fits.open(dictionary.get(filt).get('file')) as hdu :
-                hdr = hdu[0].header
-                exposure = hdr['EXPTIME']*u.s
-                # photfnu = hdr['PHOTFNU'] # the ACS images don't have PHOTFNU
-                photflam = hdr['PHOTFLAM']*u.erg/(u.cm**2)/u.AA/u.electron
-                photplam = hdr['PHOTPLAM']*u.AA
-                photfnu = ((photplam**2)*
-                           photflam/const.c).to(u.Jy*u.s/u.electron)
-                scale = hdr['D001SCAL']*u.arcsec/u.pixel
-                wcs = WCS(hdr)
-                science = hdu[0].data
+            for galaxy in sci_objs :
+                rms = dictionary.get(filt).get('rms')
+                noise = np.sqrt(science/(exposure.value) + rms**2)
+                id_string = (str(ID.data)).strip('[]')
                 
-                for galaxy in sciObjs :
-                    rms = dictionary.get(filt).get('rms')
-                    noise = np.sqrt(science/(exposure.value) + rms**2)
-                    id_string = (str(ID.data)).strip('[]')
-                    
-                    # science file
-                    outfile = '{}/cutouts/{}_ID_{}_{}.fits'.format(
-                        self.name, self.name, id_string, filt)
-                    save_cutout(ra, dec, extent*r_e*scale, science, wcs,
-                                outfile, exposure.value, photfnu.value,
-                                scale.value, rms, r_e.value[0], redshift[0],
+                # science file
+                outfile = '{}/cutouts/{}_ID_{}_{}.fits'.format(
+                    cluster, cluster, id_string, filt)
+                save_cutout(ra, dec, extent*r_e*scale, science, wcs,
+                            outfile, exposure.value, photfnu.value,
+                            scale.value, rms, r_e.value[0], redshift[0],
+                            sma[0], smb[0], pa[0])
+                
+                # noise file
+                noise_outfile = '{}/cutouts/{}_ID_{}_{}_noise.fits'.format(
+                    cluster, cluster, id_string, filt)
+                save_cutout(ra, dec, extent*r_e*scale, noise, wcs,
+                            noise_outfile, exposure.value, photfnu.value,
+                            scale.value, rms, r_e.value[0], redshift[0],
+                            sma[0], smb[0], pa[0])
+                
+                # segmentation map
+                with fits.open(segPath) as hdu :
+                    segMap = hdu[0].data
+                segmap_outfile = '{}/cutouts/{}_ID_{}_segmap.fits'.format(
+                    cluster, cluster, id_string)
+                try :
+                    save_cutout(ra, dec, extent*r_e*scale, segMap, wcs,
+                                segmap_outfile, -999, -999, scale.value,
+                                -999, r_e.value[0], redshift[0],
                                 sma[0], smb[0], pa[0])
-                    
-                    # noise file
-                    noise_outfile = '{}/cutouts/{}_ID_{}_{}_noise.fits'.format(
-                        self.name, self.name, id_string, filt)
-                    save_cutout(ra, dec, extent*r_e*scale, noise, wcs,
-                                noise_outfile, exposure.value, photfnu.value,
-                                scale.value, rms, r_e.value[0], redshift[0],
-                                sma[0], smb[0], pa[0])
-                    
-                    # segmentation map
-                    with fits.open(self.segPath) as hdu :
-                        segMap = hdu[0].data
-                    segmap_outfile = '{}/cutouts/{}_ID_{}_segmap.fits'.format(
-                        self.name, self.name, id_string)
-                    try :
-                        save_cutout(ra, dec, extent*r_e*scale, segMap, wcs,
-                                    segmap_outfile, -999, -999, scale.value,
-                                    -999, r_e.value[0], redshift[0],
-                                    sma[0], smb[0], pa[0])
-                    except OSError as error :
-                        if error.errno == errno.EEXIST :
-                            pass
+                except OSError as error :
+                    if error.errno == errno.EEXIST :
+                        pass
+    
+    return
+
+def save_filterset(cluster, filters) :
+    '''
+    Save a file containing the filters for the cluster, one per line. This
+    will be used when fitting the photometry in prospector.
+    
+    Returns
+    -------
+    None.
+    
+    '''
+    
+    filterset_file = '{}/filters.txt'.format(cluster)
+    with open(filterset_file, 'a+') as file :
+        for filt in filters :
+            file.write('hff_{}\n'.format(filt))
         
         return
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 
 import binning
-from cluster import Cluster
 import core
-import plotting as plt
+import field
 import photometry
+import plotting as plt
+import resave
 import voronoi
 
-def premain(save_filters=False, plot_curves=False) :
+def premain(save_catalogs=False, save_filters=False, plot_curves=False) :
+    '''
+    
+    
+    Parameters
+    ----------
+    save_catalogs : TYPE, optional
+        DESCRIPTION. The default is False.
+    save_filters : TYPE, optional
+        DESCRIPTION. The default is False.
+    plot_curves : TYPE, optional
+        DESCRIPTION. The default is False.
+    
+    Returns
+    -------
+    None.
+    
+    '''
+    
+    if save_catalogs :
+        resave.resave_all()
     
     if save_filters :
         core.build_filter_table(save_par=True)
@@ -16,49 +37,84 @@ def premain(save_filters=False, plot_curves=False) :
     
     return
 
-def main(binning_scheme='annuli') :
+def main(cluster, calculate_rms=False, binning_scheme='annuli') :
+    '''
     
-    # determine the final science objects for Abell 2744, based on flags
-    # in the catalogs
-    core.determine_finalObjs_w_UVJ('abell_2744', 'id', 0.308,
-                                   'abell2744clu_catalogs',
-                                   'abell2744clu_v3.9', 153, 155, 161)
     
-    # create a Cluster instance for Abell 2744
-    a2744 = initialize('a2744')
+    Parameters
+    ----------
+    cluster : TYPE
+        DESCRIPTION.
+    calculate_rms : bool
+        Flag to set if the rms values should be calculated. Default is False.
+    binning_scheme : TYPE, optional
+        DESCRIPTION. The default is 'annuli'.
     
-    # determine the rms values for each filter for Abell 2744
-    rms = a2744.determine_rms()
-    print(rms)
+    Returns
+    -------
+    None.
     
-    # save all the cutouts for all the science objects for Abell 2744
-    a2744.save_cutouts()
+    '''
     
-    # now vorbin the cutouts for each galaxy and save the resulting files
-    if binning_scheme == 'annuli' :
-        binning.bin_all('a2744/cutouts/', 'a2744/bins/')
-    else :
-        voronoi.vorbin_all('a2744/cutouts/', 'a2744/vorbins/')
+    U_filtnum, V_filtnum, J_filtnum = 153, 155, 161
     
-    # then determine the flux for every vorbin for each galaxy, saving the
-    # photometry for each galaxy into a separate file
-    photometry.determine_fluxes('a2744/bins/', 'a2744/cutouts/',
-                                'a2744/photometry/', a2744.filters)
+    if cluster == 'a370' :
+        cluster_redshift = 0.375
+        first_path, second_path = 'abell370clu_catalogs', 'abell370clu_v3.9'
+        misc = 'misc/abell370clu_misc/'
+        
+        filters = ['f275w', 'f336w', # UVIS
+                   'f435w', 'f475w', 'f606w', 'f625w', 'f814w', # ACS
+                   'f105w', 'f110w', 'f125w', 'f140w', 'f160w'] # IR
+        
+        segPath = misc + 'photometry/abell370_bcgs_out_detection_seg.fits.gz'
+        
+        psf_matched = misc + 'images/psf_matched/'
+        bcgs_out = misc + 'images/bcgs_out/'
+        paths = [psf_matched + 'abell370_bcgs_out_f275w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f336w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f435w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f606w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f814w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f105w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f125w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell370_bcgs_out_f140w_psf_bkg_drz.fits.gz',
+                 bcgs_out + 'abell370_bcgs_out_f160w_bkg_drz_masked.fits.gz']
     
-    # then on linux, move to the `hff/` directory, and run
-    # python prospector.py
-    
-    return
-
-def initialize(cluster) :
+    if cluster == 'a1063' :
+        cluster_redshift = 0.348
+        first_path, second_path = 'abell1063clu_catalogs', 'abell1063clu_v3.9'
+        misc = 'misc/abell1063clu_misc/'
+        
+        filters = ['f225w', 'f275w', 'f336w','f390w', # UVIS
+                   'f435w', 'f475w', 'f606w', 'f625w', 'f775w', 'f814w', 'f850lp', # ACS
+                   'f105w', 'f110w', 'f125w', 'f140w', 'f160w'] # IR
+        
+        segPath = misc + 'photometry/abell1063_bcgs_out_detection_seg.fits.gz'
+        
+        psf_matched = misc + 'images/psf_matched/'
+        bcgs_out = misc + 'images/bcgs_out/'
+        paths = [psf_matched + 'abell1063_bcgs_out_f275w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f336w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f435w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f606w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f814w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f105w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f125w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'abell1063_bcgs_out_f140w_psf_bkg_drz.fits.gz',
+                 bcgs_out + 'abell1063_bcgs_out_f160w_bkg_drz_masked.fits.gz']
     
     if cluster == 'a2744' :
+        cluster_redshift = 0.308
+        first_path, second_path = 'abell2744clu_catalogs', 'abell2744clu_v3.9'
+        misc = 'misc/abell2744clu_misc/'
         
         filters = ['f275w', 'f336w', # UVIS
                    'f435w', 'f606w', 'f814w', # ACS
                    'f105w', 'f125w', 'f140w', 'f160w'] # IR
         
-        misc = 'misc/abell2744clu_misc/'
+        segPath = misc + 'photometry/abell2744_bcgs_out_detection_seg.fits.gz'
+        
         psf_matched = misc + 'images/psf_matched/'
         bcgs_out = misc + 'images/bcgs_out/'
         paths = [psf_matched + 'abell2744_bcgs_out_f275w_psf_bkg_drz.fits.gz',
@@ -71,42 +127,112 @@ def initialize(cluster) :
                  psf_matched + 'abell2744_bcgs_out_f140w_psf_bkg_drz.fits.gz',
                  bcgs_out + 'abell2744_bcgs_out_f160w_bkg_drz_masked.fits.gz']
         
-        segPath = misc + 'photometry/abell2744_bcgs_out_detection_seg.fits.gz'
-        
-        finalSciObjs = cluster + '/' + cluster + '_final_objects.fits'
-        
         rms = [0.00017821459857111745, 0.00014789605532426083,
                0.0007145838788770763,  0.001578221285547029,
                0.0018031112928780836,  0.0293976362500103,
                0.002737900021748479,   0.016844529370723973,
                0.0037151459]
+    
+    if cluster == 'm416' :
+        cluster_redshift = 0.392
+        first_path, second_path = 'macs0416clu_catalogs', 'macs0416clu_v3.9'
+        misc = 'misc/macs0416clu_misc/'
         
-        a2744 = Cluster(cluster, filters, paths, segPath, finalSciObjs, rms)
+        filters = ['f225w', 'f275w', 'f336w','f390w', # UVIS
+                   'f435w', 'f475w', 'f606w', 'f625w', 'f775w', 'f814w', 'f850lp', # ACS
+                   'f105w', 'f110w', 'f125w', 'f140w', 'f160w'] # IR
         
-        return a2744
+        segPath = misc + 'photometry/macs0416_bcgs_out_detection_seg.fits.gz'
+        
+        psf_matched = misc + 'images/psf_matched/'
+        bcgs_out = misc + 'images/bcgs_out/'
+        paths = [psf_matched + 'macs0416_bcgs_out_f275w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f336w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f435w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f606w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f814w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f105w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f125w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0416_bcgs_out_f140w_psf_bkg_drz.fits.gz',
+                 bcgs_out + 'macs0416_bcgs_out_f160w_bkg_drz_masked.fits.gz']
+    
+    if cluster == 'm717' :
+        cluster_redshift = 0.5458
+        first_path, second_path = 'macs0717clu_catalogs', 'macs0717clu_v3.9'
+        misc = 'misc/macs0717clu_misc/'
+        
+        filters = ['f225w', 'f275w', 'f336w','f390w', # UVIS
+                   'f435w', 'f475w', 'f555w', 'f606w', 'f625w', 'f775w', 'f814w', 'f850lp', # ACS
+                   'f105w', 'f110w', 'f125w', 'f140w', 'f160w'] # IR
+        
+        segPath = misc + 'photometry/macs0717_bcgs_out_detection_seg.fits.gz'
+        
+        psf_matched = misc + 'images/psf_matched/'
+        bcgs_out = misc + 'images/bcgs_out/'
+        paths = [psf_matched + 'macs0717_bcgs_out_f275w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f336w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f435w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f606w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f814w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f105w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f125w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs0717_bcgs_out_f140w_psf_bkg_drz.fits.gz',
+                 bcgs_out + 'macs0717_bcgs_out_f160w_bkg_drz_masked.fits.gz']
+    
+    if cluster == 'm1149' :
+        cluster_redshift = 0.543
+        first_path, second_path = 'macs1149clu_catalogs', 'macs1149clu_v3.9'
+        misc = 'misc/macs1149clu_misc/'
+        
+        filters = ['f225w', 'f275w', 'f336w','f390w', # UVIS
+                   'f435w', 'f475w', 'f555w', 'f606w', 'f625w', 'f775w', 'f814w', 'f850lp', # ACS
+                   'f105w', 'f110w', 'f125w', 'f140w', 'f160w'] # IR
+        
+        segPath = misc + 'photometry/macs1149_bcgs_out_detection_seg.fits.gz'
+        
+        psf_matched = misc + 'images/psf_matched/'
+        bcgs_out = misc + 'images/bcgs_out/'
+        paths = [psf_matched + 'macs1149_bcgs_out_f275w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f336w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f435w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f606w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f814w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f105w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f125w_psf_bkg_drz.fits.gz',
+                 psf_matched + 'macs1149_bcgs_out_f140w_psf_bkg_drz.fits.gz',
+                 bcgs_out + 'macs1149_bcgs_out_f160w_bkg_drz_masked.fits.gz']
+    
+    # determine the final science objects, based on flags in the catalogs
+    core.determine_finalObjs_w_UVJ(cluster, 'id', cluster_redshift,
+                                   first_path, second_path,
+                                   U_filtnum, V_filtnum, J_filtnum,
+                                   plot_all=True, plot_uvj=True,
+                                   write_final_objs=True, write_regions=True)
+    
+    # save a file containing information about the filterset for the cluster
+    field.save_filterset(cluster, filters)
+    
+    # determine the rms values for each filter for Abell 2744
+    if calculate_rms :
+        rms = field.determine_rms(segPath, paths)
     else :
-        return None
-
-# determine_finalObjs_w_UVJ('abell_2744', 'id', 0.308, 'abell2744clu_catalogs',
-#                           'abell2744clu_v3.9', 153, 155, 161)
-# determine_finalObjs_w_UVJ('abell_370', 'id', 0.375, 'abell370clu_catalogs',
-#                           'abell370clu_v3.9', 153, 155, 161)
-# determine_finalObjs_w_UVJ('abell_S1063', 'id', 0.348, 'abell1063clu_catalogs',
-#                           'abell1063clu_v3.9', 153, 155, 161)
-# determine_finalObjs_w_UVJ('macs_0416', 'id', 0.392, 'macs0416clu_catalogs',
-#                           'macs0416clu_v3.9', 153, 155, 161)
-# determine_finalObjs_w_UVJ('macs_0717', 'id', 0.5458, 'macs0717clu_catalogs',
-#                           'macs0717clu_v3.9', 153, 155, 161)
-# determine_finalObjs_w_UVJ('macs_1149', 'id', 0.543, 'macs1149clu_catalogs',
-#                           'macs1149clu_v3.9', 153, 155, 161)
-
-# still necessary?
-# determine_final_objects('abell_370', 'id', 0.375)
-# determine_final_objects('abell_S1063', 'id', 0.348)
-# determine_final_objects('abell_2744', 'id', 0.308, plot=True)
-# determine_final_objects('macs_0416', 'id', 0.392)
-# determine_final_objects('macs_0717', 'id', 0.5458)
-# determine_final_objects('macs_1149', 'id', 0.543)
-
-# still necessary?
-# combine_UVJ('abell2744clu_catalogs', 'abell2744clu_v3.9', 153, 155, 161)
+        rms = rms
+    
+    # save all the cutouts for all the science objects
+    final_objs_path = '{}/{}_final_objects.fits'.format(cluster, cluster)
+    field.save_cutouts(cluster, final_objs_path, filters, rms, paths, segPath)
+    
+    # now vorbin the cutouts for each galaxy and save the resulting files
+    if binning_scheme == 'annuli' :
+        binning.bin_all(cluster)
+    else :
+        voronoi.vorbin_all(cluster)
+    
+    # then determine the flux for every vorbin for each galaxy, saving the
+    # photometry for each galaxy into a separate file
+    photometry.determine_fluxes(cluster, filters)
+    
+    # then on linux, move to the `hff/` directory, and run
+    # python prospector.py
+    
+    return
